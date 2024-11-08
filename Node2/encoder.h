c2140075b8c339ca607de1b5d8816f87b3f3da8e
William <60541285+autoHammer@users.noreply.github.com>
2024-11-08 14:06:26 +0100

    Add comments to encoder.h

--- Node2/encoder.h
+++ Node2/encoder.h
@@ -6,10 +6,21 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+/*
+* Initializes the encoder IO
+*/
 void encoder_init();
 
+/*
+* Read encoder index value from register
+* Will reset position whenever position is negative
+* @return index value
+*/
 uint32_t encoder_read();
 
+/*
+* Set encoder position to 0.
+*/
 void encoder_reset();
 
 #endif /* ENCODER_H */
